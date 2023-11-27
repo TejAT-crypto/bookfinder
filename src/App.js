dfd3e0f4ec17c0392370f1e531610cf84e67d163
TejAT-crypto <80670905+TejAT-crypto@users.noreply.github.com>
2023-11-27 20:50:51 +0530

    frontend repo created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { Routes, Route, useLocation } from "react-router-dom";
+import React, { useEffect } from "react";
 import './App.css';
+import UserRegister from "./pages/User/UserRegister";
 
-function App() {
+const App = () => {
+  const [isSignedIn, setIsSignedIn] = React.useState(sessionStorage.getItem('isSignedIn') === 'true')
+  
+  const signin = () => {
+    setIsSignedIn(true)
+    sessionStorage.setItem('isSignedIn', 'true')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element = {
+          <UserRegister signin = {signin} />
+        } />
+      </Routes>
     </div>
   );
 }
