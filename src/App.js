632578256c60dbd92d05268179bb47621be6efca
TejAT-crypto <80670905+TejAT-crypto@users.noreply.github.com>
2023-11-28 00:10:05 +0530

    Refreshed login and register pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import React, { useEffect } from "react";
 import './App.css';
 import UserRegister from "./pages/User/UserRegister";
+import UserLogin from "./pages/User/UserLogin";
 
 const App = () => {
   const [isSignedIn, setIsSignedIn] = React.useState(sessionStorage.getItem('isSignedIn') === 'true')
@@ -16,6 +17,9 @@ const App = () => {
       <Routes>
         <Route path="/" element = {
           <UserRegister signin = {signin} />
+        } />
+        <Route path="/login" element = {
+          <UserLogin signin = {signin} />
         } />
       </Routes>
     </div>
